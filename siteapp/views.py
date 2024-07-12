fc4acf00f959f436b4ed557471be8828f7e7b4c7
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-07-12 20:35:09 +0545

    deploey

--- siteapp/views.py
+++ siteapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello from dipendra Tamang its keep on testing \n this website is being developed by dipendra using python django ")
+    return HttpResponse("Hello from dipendra Tamang, its keep on testing \n this website is being developed by dipendra using python django ")
