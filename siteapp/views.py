4988351de5ca3d92474f1298a142f2b0869b3d1d
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-07-13 20:06:43 +0545

    adding static folders

--- siteapp/views.py
+++ siteapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello from Dipen,This websites is being developed by dipendra using python django ")
+    return HttpResponse("Hello from Dipen,This websites ss is being developed by dipendra using python django ")
