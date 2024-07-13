2d87c65e2eeb800f897cfce8e3eaba59cb0efaca
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-07-13 19:11:02 +0545

    seconf

--- dipendraproject/settings.py
+++ dipendraproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wh#l9l)gftr4-a@eo$zrn=!+@ude&3l)elfjdrx7hz^x^m&4ah
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dipendratamang.com.np']
+ALLOWED_HOSTS = ['dipendratamang.com.np','127.0.0.1']
 
 
 # Application definition
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
