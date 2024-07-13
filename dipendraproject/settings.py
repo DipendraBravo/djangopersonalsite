33547127feef050b97ec5b9c4086f6eef72edaca
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-07-13 19:15:21 +0545

    changes

--- dipendraproject/settings.py
+++ dipendraproject/settings.py
@@ -12,9 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import pytz
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
+# Example of using pytz
+TIME_ZONE = 'Asia/Kathmandu'  # Set your appropriate time zone
+USE_TZ = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
