59dfc983c773dac8b8d9b20b38b2edb5fc108f51
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-08-24 14:40:34 +0545

    adding the template

--- siteapp/views.py
+++ siteapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello from Dipen,This websites ss is being developed by dipendra using python django ")
+    return render(request, 'home/index.html')
