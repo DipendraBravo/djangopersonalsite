36e8ffeca12d282a6f9f960e843146ddb2af079f
Dipendra Tamang <dipendra.tamang9@gmail.com>
2024-09-08 08:02:02 +0545

    adding view

--- siteapp/views.py
+++ siteapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from siteapp import models
+from siteapp.models import Skill, Project, WorkExperience
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/index.html')
+    skills = Skill.objects.all()
+    projects = Project.objects.all()
+    work_experiences = WorkExperience.objects.all()
+    return render(request, 'home/index.html',{
+        'skills':skills,'projects':projects,'work_experiences':work_experiences
+    })
 
 
